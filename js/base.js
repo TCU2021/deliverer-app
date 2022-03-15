b2696f9acd4274efe411ff2d420e8c83c8141c9e
tusake <8463894+tusake@user.noreply.gitee.com>
2022-03-15 21:41:50 +0800

    修改接口地址

--- js/base.js
+++ js/base.js
@@ -1,3 +1,3 @@
 // export const baseUrl="http://localhost:3000/"
-export const baseUrl="http://192.168.31.130:3000/"
-export const baseUrl2="http://81.70.2.91:3000/"
+// export const baseUrl="http://192.168.31.130:3000/"
+export const baseUrl="http://81.70.2.91:3000/"
